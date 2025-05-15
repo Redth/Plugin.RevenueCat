271b2f8553e62135c6be0fb9406dac6a2ae39cc7
redth <jondick@gmail.com>
2025-05-15 19:56:45 -0400

    Start adding webview client

---
+++ Plugin.RevenueCat.WebView/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Plugin.RevenueCat.WebView;
+// All the code in this file is only included on Tizen.
+public class PlatformClass1
+{
+}
---
+++ Plugin.RevenueCat.WebView/RevenueCatGeneric.cs
@@ -0,0 +1,57 @@
+﻿using Plugin.RevenueCat.Api;
+using Plugin.RevenueCat.Models;
+
+namespace Plugin.RevenueCat;
+
+public class RevenueCatGeneric(IRevenueCatApiV1 RevenueCatApiVi) : IRevenueCatManager
+{
+	public string? ApiKey { get; }
+
+	public event EventHandler<CustomerInfoUpdatedEventArgs>? CustomerInfoUpdated;
+
+	public async Task<CustomerInfo?> GetCustomerInfoAsync(bool force)
+	{
+		if (string.IsNullOrEmpty(UserId))
+			return default;
+
+		var c = await RevenueCatApiVi.GetOrCreateCustomer(UserId);
+		
+		var customerInfo = new CustomerInfo();
+
+		customerInfo.CustomerInfoRequestDate = c.RequestDate;
+		customerInfo.RequestDate = c.RequestDate ?? DateTimeOffset.Now;
+		customerInfo.Subscriber = c.Subscriber ?? new Subscriber();
+
+		return customerInfo;
+	}
+
+	public async Task<Offering?> GetOfferingAsync(string offeringIdentifier)
+	{
+		var c = await RevenueCatApiVi.GetOfferings(UserId);
+
+		return c.Offerings?.FirstOrDefault(c => c.Identifier == offeringIdentifier); ;
+	}
+
+	public void Initialize()
+	{
+	}
+
+	public string UserId { get; private set; } = string.Empty;
+
+	public Task<CustomerInfo?> LoginAsync(string userId)
+	{
+		UserId = userId;
+
+		return GetCustomerInfoAsync(true);
+	}
+
+	public Task<CustomerInfo?> PurchaseAsync(object? platformContext, string offeringIdentifier, string packageIdentifier)
+	{
+		
+	}
+
+	public Task<CustomerInfo?> RestoreAsync()
+	{
+		return GetCustomerInfoAsync(true);
+	}
+}
